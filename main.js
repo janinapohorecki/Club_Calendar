6bcf1969318c4a550d3ae53130e097ff98f51637
EmersonFlom <emerson.flom@gmail.com>
2019-03-06 19:27:44 +0000

    Initialized main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+const express = require('express')
+const app = express()
+const port = 3000
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
