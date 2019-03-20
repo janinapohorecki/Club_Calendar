8668563b3abb97aec06d94aacc7ab52bce330f7e
Emerson Flom <emerson.flom@gmail.com>
2019-03-20 16:29:34 -0500

    Initial version of database

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://localhost:27017/ServerDB";
+
+// Connect to the db
+MongoClient.connect(uri, {useNewUrlParser:true}, function(err, db) {
+  if(err) { return console.dir(err); }
+
+  var collection = db.collection('test');
+  var doc1 = {'hello':'doc1'};
+  var doc2 = {'hello':'doc2'};
+  var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+
+  collection.insert(doc1);
+
+  collection.insert(doc2, {w:1}, function(err, result) {});
+
+  collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+
+});
