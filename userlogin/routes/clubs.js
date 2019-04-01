8d138ea084e0c4edc90e316019e71763a8f473c6
Emerson Flom <emerson.flom@gmail.com>
2019-04-01 16:31:41 -0500

    Added add club functionality and tested

--- userlogin/routes/clubs.js
+++ userlogin/routes/clubs.js
@@ -2,98 +2,62 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-// Load Event model
-const Event = require('../models/Club');
+// Load Club model
+const Club = require('../models/Club');
 
-// Create Event Page
+// Create Club Page
 router.get('/createclub', (req, res) => res.render('createclub'));
 
-// Create event
+// Create club
 router.post('/createclub', (req, res) => {
-  const { clubID, name, clubType, followers, clubEmail } = req.body;
+  const { clubID, name, clubType, clubEmail, followers } = req.body;
   console.log(req.body);
   let errors = [];
 
   if (!clubID || !name || !clubType || !clubEmail) {
     errors.push({ msg: 'Please enter all fields' });
   }
-  else {
-      // Creating date variables for event
-      var d1 = new Date(StartTime);
-      var d2 = new Date(EndTime);
-      
-      var currentDate = new Date();
-      var check = new Date();
-      check.setDate(d1.getDate()-1);
-      
-    // Checking if the dates/times are correct
-    if (check < currentDate) {
-      errors.push({ msg: 'You must create an event at least one day in advance' });
-    }
-    else {
-        if (d1.getDate() > d2.getDate()) {
-          errors.push({ msg: 'Cannot create event with start date before the current date'});
-        }
-      else {
-        if(!(d1.getDate() < d2.getDate())) {
-          if(!(d1.getTime() < d2.getTime()))
-          {
-            errors.push({ msg: 'Cannot create an event with an end time before the start time'});
-          }
-        }
-      }
-    }
-  }
   
   if (errors.length > 0) {
-    res.render('createevent', {
+    res.render('createclub', {
       errors,
-      clubID, 
-      userID, 
-      eventID, 
-      name, 
-      description, 
-      StartTime, 
-      EndTime, 
-      Approved
+      clubID,
+      name,
+      clubType,
+      clubEmail,
+      followers 
     });
   } else {
-    Event.findOne({ eventID: eventID }).then(event => { // mongoose method that finds one user
-      if (event) {
-        errors.push({ msg: 'Event already exists' });
-        res.render('createevent', {
+    Club.findOne({ clubID: clubID }).then(club => { // mongoose method that finds one user
+      if (club) {
+        errors.push({ msg: 'Club already exists' });
+        res.render('createclub', {
           errors,
-          clubID, 
-          userID, 
-          eventID, 
-          name, 
-          description, 
-          StartTime, 
-          EndTime, 
-          Approved
-        });
+          clubID,
+          name,
+          clubType,
+          clubEmail,
+          followers 
+       });
       } else {
-        const newEvent = new Event({
-          clubID, 
-          userID, 
-          eventID, 
-          name, 
-          description, 
-          StartTime, 
-          EndTime, 
-          Approved
+        const newClub = new Club({
+          clubID,
+          name,
+          clubType,
+          clubEmail,
+          followers 
         });
 
-        newEvent
+        newClub
           // Save User
           .save()
-          .then(event => {
+          .then(club => {
             req.flash(
               'success_msg',
-              'Your event was submitted and is waiting for approval'
+              'Club added'
             );
             // Redirecting user to login page!
-            res.redirect('/users/register'); // Should redirect back to calendar
+            res.redirect('createclub'); // Should redirect back to calendar
           })
           .catch(err => console.log(err));
       }
