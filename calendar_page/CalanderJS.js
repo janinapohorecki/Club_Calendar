721b6ca50402c8fba12e621942e746c668cc2bd5
Matthew Behnke <behnke2424@live.com>
2019-04-01 14:32:03 -0600

    SideBar is Done

--- calendar_page/CalanderJS.js
+++ calendar_page/CalanderJS.js
@@ -2,3 +2,9 @@ function toggleSidebar() {
     "use strict";
     document.getElementById("sidebar").classList.toggle('active');
 }
+
+document.getElementById("Social-Drop").onclick = function() {SocialDropFun()};
+
+function SocialDrop() {
+    document.getElementById("Social-Sublist").classList.toggle("show");
+}
