58cf966ebf9841b623eb94672b60612baeaa1b09
Jane Poho <janepohorecka@gmail.com>
2019-04-10 14:50:52 -0600

    Brought up to date with master

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const { ensureAuthenticated } = require('../config/auth');
 router.get('/', (req, res) => res.render('homepage'));
 
 router.get('/dashboard', (req, res) => res.render('dashboard'));
-/*
+
+router.get('/testingDashboard', (req, res) => res.render('testingDashboard'));
+
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
     user: req.user
   })
 );
-*/
+
 module.exports = router;
