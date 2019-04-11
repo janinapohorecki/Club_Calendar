bb48a6031ca7b42f40ac5fa46a4042f631ff4cb1
Emerson Flom <emerson.flom@gmail.com>
2019-04-11 15:50:24 -0500

    Edit mod.ejs and mod.js, currently doesn't connect between the two, have read from the database

--- routes/moderator.js
+++ routes/moderator.js
@@ -5,105 +5,13 @@ const passport = require('passport');
 // Load Event model
 const Event = require('../models/Event');
 
-// Create Event Page
 router.get('/moderator', (req, res) => res.render('moderator'));
-// Create event
-router.post('/moderator', (req, res) => {
-  const { clubID, userID, eventID, name, description, date, startTime, endTime, Approved } = req.body;
-  console.log(req.body);
-  let errors = [];
 
-// All above is good, don't change please
+var event1 = Event.find({Approved: false});
 
-
-  if (!clubID || !userID || !eventID || !name || !description || !date || !startTime || !endTime) {
-    errors.push({ msg: 'Please enter all fields' });
-  }
-  else {
-      var d1 = new Date(Date.parse(date+"T"+startTime));
-      var d2 = new Date(Date.parse(date+"T"+endTime));
-      d1.setHours(d1.getHours()-5);
-      d2.setHours(d2.getHours()-5);
-      var currentDate = new Date();
-      var check = new Date();
-      check.setDate(d1.getDate()-1);
-      
-    // Checking if the dates/times are correct
-    if (check < currentDate) {
-      errors.push({ msg: 'You must create an event at least one day in advance' });
-    }
-    else {
-        if (d1.getDate() > d2.getDate()) {
-          errors.push({ msg: 'Cannot create event with start date before the current date'});
-        }
-      else {
-        if(!(d1.getDate() < d2.getDate())) {
-          if(!(d1.getTime() < d2.getTime()))
-          {
-            errors.push({ msg: 'Cannot create an event with an end time before the start time'});
-          }
-        }
-      }
-    }
-  }
-  var StartTime=d1;
-  var EndTime=d2;
-  if (errors.length > 0) {
-    res.render('createevent', {
-      errors,
-      clubID, 
-      userID, 
-      eventID, 
-      name, 
-      description, 
-      date, 
-      startTime,
-      endTime, 
-      Approved
-    });
-  } else {
-    Event.findOne({ eventID: eventID }).then(event => { // mongoose method that finds one user
-      if (event) {
-        errors.push({ msg: 'Event already exists' });
-        res.render('createevent', {
-          errors,
-          clubID, 
-          userID, 
-          eventID, 
-          name, 
-          description, 
-          date, 
-          startTime,
-          endTime, 
-          Approved
-        });
-      } else {
-        const newEvent = new Event({
-          clubID, 
-          userID, 
-          eventID, 
-          name, 
-          description, 
-          StartTime, 
-          EndTime, 
-          Approved
-        });
-
-        newEvent
-          // Save User
-          .save()
-          .then(event => {
-            req.flash(
-              'success_msg',
-              'Your event was submitted and is waiting for approval'
-            );
-            // Redirecting user to login page!
-            res.redirect('/dashboard'); // Should redirect back to calendar
-          })
-          .catch(err => console.log(err));
-      }
-    });
-  }
+router.post('/moderator', (req, res, next) => {                        
+        res.render('/moderator', {
+        event: event1
+    });   
 });
-
 module.exports = router;
