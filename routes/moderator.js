9aff5f2c060a935ffb50b1c5e075d1c2e630c740
Emerson Flom <emerson.flom@gmail.com>
2019-04-12 15:19:40 -0500

    moderator.ejs successfully queries the database

--- routes/moderator.js
+++ routes/moderator.js
@@ -7,11 +7,12 @@ const Event = require('../models/Event');
 
 router.get('/moderator', (req, res) => res.render('moderator'));
 
-var event1 = Event.find({Approved: false});
-
-router.post('/moderator', (req, res, next) => {                        
-        res.render('/moderator', {
-        event: event1
-    });   
+var query = Event.where({Approved:true});
+query.find(function(err,event) {
+  let errors = [];
+  if(err) err => console.log(err);
+  else {
+      console.log(event);
+  }
 });
 module.exports = router;
