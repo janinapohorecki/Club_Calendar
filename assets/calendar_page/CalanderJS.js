f97ed9f1e419c6cef657db9520d77cc42f9d9da8
Matthew Behnke <behnke2424@live.com>
2019-04-12 16:12:43 -0600

    I made the mod.html page

--- assets/calendar_page/CalanderJS.js
+++ assets/calendar_page/CalanderJS.js
@@ -5,6 +5,12 @@ function toggleSidebar() {
 
 document.getElementById("Social-Drop").onclick = function() {SocialDropFun()};
 
-function SocialDrop() {
+/*function SocialDrop() {
     document.getElementById("Social-Sublist").classList.toggle("show");
-}
+}*/
+
+$(document).ready(function(){
+  $("#PAC-checkbox").click(function(){
+    $("#PAC-EVENTS").toggle();
+  });
+});
