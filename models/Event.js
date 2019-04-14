4fe22fc3d9077d74b84d66963b35c1f6b4529f0d
Emerson Flom <emerson.flom@gmail.com>
2019-04-14 14:52:05 -0600

    Moderator page displays club name

--- models/Event.js
+++ models/Event.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
   club: {
-    type: Number,
+    type: String,
     required: true,
     unique: true
   },
--- routes/moderator.js
+++ routes/moderator.js
@@ -12,12 +12,8 @@ query.select('-_id name description StartTime EndTime club')
 query.find(function(err,event) {
   if(err) console.log(err);
   else {
-          var query2 = Club.where({clubID: event.club });
-          query2.select('-_id name')
-          query2.find(function(err,club) {
             router.get('/moderator', function(req, res) {
-            res.render('moderator', {events: event, clubs: club});
+            res.render('moderator', {events: event});
           })
-      })
 }});
 module.exports = router;
