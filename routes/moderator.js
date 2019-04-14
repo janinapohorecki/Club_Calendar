407227ae7c413dcb08e214ee2f34aa2d5c7c7b03
Jane Poho <janepohorecka@gmail.com>
2019-04-14 12:53:28 -0600

    Merged with master

--- routes/moderator.js
+++ routes/moderator.js
@@ -10,9 +10,8 @@ query.select('-_id name description StartTime EndTime')
 query.find(function(err,event) {
   if(err) console.log(err);
   else {
-          //obj = JSON.parse(json);
           router.get('/moderator', function(req, res) {
-          res.render('moderator', {blah: event});
+          res.render('moderator', {events: event});
       })
   }
 });
