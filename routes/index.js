8e0021479f72ca051613b7167b8913256a0cde59
Emerson Flom <emerson.flom@gmail.com>
2019-04-15 13:10:28 -0600

    Updated index.js to pull events whenever either the homepage or dashboard is called

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,24 @@ const Event = require('../models/Event');
 
 var query1 = Event.where({Approved:true});
 query1.select('-_id club name StartTime EndTime description location')
-query1.find(function(err,event) {
-  router.get('/', (req, res) => res.render('homepage', {
-    events : event
+router.get('/', function(req,res) {
+  query1.find(function(err,event) {
+    if(err) console.log(err);
+    else res.render('homepage', {events : event});
   })
-)});
+});
+
+var query2 = Event.where({Approved:true});
+query2.select('-_id club name StartTime EndTime description location')
+router.get('/dashboard', ensureAuthenticated, function(req,res) {
+  query2.find(function(err,event) {
+    if(err) console.log(err);
+    else res.render('dashboard', {
+      user : req.user,
+      events : event
+    });
+  })
+});
 
 // Dashboard
 var query2 = Event.where({Approved:true});
